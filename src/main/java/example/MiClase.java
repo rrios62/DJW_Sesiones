596d4ede44c4fb62d07f5ce847fd6bfff2a726a6
Ricardo Rios Donato <rrios62@gmail.com>
2023-06-19 13:22:03 -0600

    Se agregan las dependencias UNIT y JaCoCo

--- src/main/java/example/MiClase.java
+++ src/main/java/example/MiClase.java
@@ -3,26 +3,32 @@ package example;
 import java.util.Scanner;
 
 public class MiClase {
-    public static void main(String[] args) {
-        Scanner sc;
-        int a,b,c;
-        sc=new Scanner(System.in);
+
+    public static int numMayor(int a,int b, int c){
+        /*Scanner sc;
+        int a, b, c;
+        sc = new Scanner(System.in);
 
         System.out.println("Ingrese el primero numero");
-            a=sc.nextInt();
+        a = sc.nextInt();
         System.out.println("Ingrese el segundo numero");
-            b=sc.nextInt();
+        b = sc.nextInt();
         System.out.println("Ingrese el tercer numero");
-            c=sc.nextInt();
-        if(a>b && a>c){
-            System.out.println("El numero mayor es: "+a);
-        }else if(c>b){
-            System.out.println("El numero mayor es: "+c);
-        }else {
-            System.out.println("El numero mayor es: "+b);
+        c = sc.nextInt();*/
+        if (a > b && a > c) {
+            System.out.println("El numero mayor es A: " + a);
+            return a;
+        } else if (c > b) {
+            System.out.println("El numero mayor es C: " + c);
+            return c;
+        } else {
+            System.out.println("El numero mayor es B: " + b);
+            return b;
         }
+    }
+    public static void main(String[] args) {
 
-
+        numMayor(5,6,7);
     }
 
 
--- src/test/java/example/MiClaseTest.java
+++ src/test/java/example/MiClaseTest.java
@@ -1,17 +1,54 @@
 package example;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MiClaseTest {
-
-    @BeforeEach
-    void setUp() {
+    @Test
+    public void testNumero_mayor_caso1() {
+        int a = 5;
+        int b = 3;
+        int c = 7;
+        MiClase instance = new MiClase();
+        int expResult = 7;
+        int result = instance.numMayor(a, b, c);
+        assertEquals(expResult, result);
     }
 
     @Test
-    void main() {
+    public void testNumero_mayor_caso2() {
+        int a = 5;
+        int b = 3;
+        int c = 4;
+        MiClase instance = new MiClase();
+        int expResult = 5;
+        int result = instance.numMayor(a, b, c);
+        assertEquals(expResult, result);
+    }
+
+    @Test
+    public void testNumero_mayor_caso3() {
+        int a = 5;
+        int b = 7;
+        int c = 6;
+        MiClase instance = new MiClase();
+        int expResult = 7;
+        int result = instance.numMayor(a, b, c);
+        assertEquals(expResult, result);
+    }
+
+    @Test
+    public void testNumero_mayor_caso4() {
+        int a = 5;
+        int b = 7;
+        int c = 9;
+        MiClase instance = new MiClase();
+        int expResult = 9;
+        int result = instance.numMayor(a, b, c);
+        assertEquals(expResult, result);
+    }
+
+    public void testNumMayor() {
     }
 }
